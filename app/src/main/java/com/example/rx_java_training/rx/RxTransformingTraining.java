6c6c9f151aecc5876dd570d5399e9998330a47c3
Artem Shabunin <shuba.2001@inbox.ru>
2025-04-04 23:10:35 +0300

    Implement RxTransformingTraining

--- app/src/main/java/com/example/rx_java_training/rx/RxTransformingTraining.java
+++ app/src/main/java/com/example/rx_java_training/rx/RxTransformingTraining.java
@@ -25,7 +25,7 @@ public class RxTransformingTraining {
      * преобразованные из чисел в {@code intObservable}
      */
     public Observable<String> transformIntToString(Observable<Integer> intObservable) {
-        throw new NotImplementedException();
+        return intObservable.map(String::valueOf);
     }
 
     /**
@@ -37,7 +37,7 @@ public class RxTransformingTraining {
      * {@code idObservable}
      */
     public Observable<Entity> requestEntityById(Observable<Integer> idObservable) {
-        throw new NotImplementedException();
+        return idObservable.flatMap(this::requestApiEntity);
     }
 
     /**
@@ -49,7 +49,7 @@ public class RxTransformingTraining {
      * поток имён объединённых первой буквой в имени
      */
     public Observable<GroupedObservable<Character, String>> distributeNamesByFirstLetter(Observable<String> namesObservable) {
-        throw new NotImplementedException();
+        return namesObservable.groupBy(name -> name.charAt(0));
     }
 
     /**
@@ -61,7 +61,7 @@ public class RxTransformingTraining {
      * @return {@code Observable} который эммитит списки чисел из {@code intObservable}
      */
     public Observable<List<Integer>> collectsIntsToLists(int listsSize, Observable<Integer> intObservable) {
-        throw new NotImplementedException();
+        return intObservable.buffer(listsSize);
     }
 
     /* Вспомогательные методы */
